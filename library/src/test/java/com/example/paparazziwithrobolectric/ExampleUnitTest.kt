ac47b23e89b62c28969fa40298f794d3d1238ecd
takahirom <takam.dev@gmail.com>
2022-05-07 00:26:28 +0900

    Add paparazzi and robolectric

--- library/src/test/java/com/example/paparazziwithrobolectric/ExampleUnitTest.kt
+++ library/src/test/java/com/example/paparazziwithrobolectric/ExampleUnitTest.kt
@@ -1,15 +1,24 @@
 package com.example.paparazziwithrobolectric
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import app.cash.paparazzi.Paparazzi
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
+    @get:Rule
+    val paparazzi = Paparazzi()
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
